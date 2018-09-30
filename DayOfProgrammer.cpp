38ba5aca2a5c94dc490692c9ad2cf258a132c2ec
sai <saikrishna25101999@gmail.com>
2018-09-30 15:08:08 +0530

    day13-4th

---
+++ DayOfProgrammer.cpp
@@ -0,0 +1,77 @@
+/*
+Marie invented a Time Machine and wants to test it by time-traveling to visit Russia on the Day of the Programmer (the 256th day of the year) during a year in the inclusive range from 1700 to 2700.
+
+From 1700 to 1917, Russia's official calendar was the Julian calendar; since 1919 they used the Gregorian calendar system. The transition from the Julian to Gregorian calendar system occurred in 1918, when the next day after January 31 st was February 14 th. This means that in 1918, February 14th was the 32nd day of the year in Russia.
+
+In both calendar systems, February is the only month with a variable amount of days; it has 29 days during a leap year, and 28 days during all other years. In the Julian calendar, leap years are divisible by ; in the Gregorian calendar, leap years are either of the following:
+
+Divisible by 400.
+Divisible by 4 and not divisible by 100.
+Given a year, y, find the date of the 256th day of that year according to the official Russian calendar during that year. Then print it in the format dd.mm.yyyy, where dd is the two-digit day, mm is the two-digit month, and yyyy is y.
+
+For example, the given year=1984.1984  is divisible by 4, so it is a leap year. The 256th day of a leap year after 1918 is September 12, so the answer is 12.09.1984.
+
+Function Description
+
+Complete the dayOfProgrammer function in the editor below. It should return a string representing the date of the 256th day of the year given.
+
+dayOfProgrammer has the following parameter(s):
+
+year: an integer
+Sample Input 0
+
+2017
+Sample Output 0
+
+13.09.2017
+Sample Input 1
+
+2016
+Sample Output 1
+
+12.09.2016
+Sample Input 2
+
+1800
+Sample Output 2
+
+12.09.1800
+Explanation 2
+
+Since 1800 is leap year. Day lies on 12 September.
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+// Complete the dayOfProgrammer function below.
+string dayOfProgrammer(int year) {
+    int y=year;
+    if(year == 1700 || year == 1800 || year == 1900)
+    {
+    return "12.09.";}
+    
+   
+    
+    if(year %4==0)
+    {
+        if(year%100 !=0)
+            return "12.09.";
+    }
+    if(year %400 ==0)
+        return "12.09.";
+    if(year==1918)
+        return "26.09.";
+    return "13.09.";
+
+
+}
+int main()
+{
+	int year;
+	cin>>year;
+	cout<<dayOfProgrammer(year)<<year;
+}
