16e886af22be7e5795c5d03edd12e90e829a72c9
sai <saikrishna25101999@gmail.com>
2018-11-03 00:27:01 +0530

    day46-2nd

---
+++ EnormousInputTest.cpp
@@ -0,0 +1,41 @@
+/*
+All submissions for this problem are available.
+The purpose of this problem is to verify whether the method you are using to read input data is sufficiently fast to handle problems branded with the enormous Input/Output warning. You are expected to be able to process at least 2.5MB of input data per second at runtime.
+
+Input
+The input begins with two positive integers n k (n, k<=107). The next n lines of input contain one positive integer ti, not greater than 109, each.
+
+Output
+Write a single integer to output, denoting how many integers ti are divisible by k.
+
+Example
+Input:
+7 3
+1
+51
+966369
+7
+9
+999996
+11
+
+Output:
+4
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,k;
+	cin>>n>>k;
+	long a,c=0;
+	for(int i=0;i<n;i++)
+	{
+		cin>>a;
+		if(a%k==0)
+		{
+			c+=1;
+		}
+	}
+	cout<<c;
+}
