dfa1986b341e1604344d921e5e065a8d64eb6774
sai <saikrishna25101999@gmail.com>
2018-11-11 21:43:01 +0530

    day55-1st

---
+++ NumberMirror.cpp
@@ -0,0 +1,24 @@
+/*
+Write a program that accepts a number, n, and outputs the same.
+
+Input
+The only line contains a single integer.
+
+Output
+Output the answer in a single line.
+
+Constraints
+0 = n = 105
+Sample Input
+123
+Sample Output
+123
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	long n;
+	cin>>n;
+	cout<<n;
+}
